01d737d404bb56855a95c1a2d1ff45398cee1f94
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-09-23 14:36:50 +0200

    Remove recursive forks in favor of while statement

--- app/models/queue_manager.rb
+++ app/models/queue_manager.rb
@@ -3,7 +3,7 @@ class QueueManager
   MAXIMUM_FORKS = 5 # Sets maximum of concurrent forks allowed to start
 
   def run
-    
+
     redis = RedisInterface.new
     forks_count = redis.get("dFile:forks") || "0"
     if forks_count.to_i > MAXIMUM_FORKS
@@ -15,89 +15,89 @@ class QueueManager
     # Fork process to allow calling method to finish
     pid = fork
     if pid.nil?
+
       Rails.logger.info "Created a new instance of queue manager (fork)"
 
       redis = RedisInterface.new
       redis.incr("dFile:forks")
 
-      # Check if there are any running processes
-      running_keys = redis.keys("dFile:processes:*:state:running")
-      if running_keys && running_keys.count >= MAXIMUM_PROCESSES
-        Rails.logger.info "There are already processes running: #{running_keys.to_json} , aborting!"
-        exit_fork
-      end
-
-      # Check if there are any queued processes
-      queued_keys = redis.keys("dFile:processes:*:state:queued")
-      if queued_keys.empty?
-        Rails.logger.info "There are no queued processes, aborting!"
-        exit_fork
-      end
-
-      # Parse queued processes
-      queued_processes = []
-      queued_keys.each do |key|
-        process_id = redis.get(key)
-
-        params = redis.get("dFile:processes:#{process_id}:params")
-        if params.nil?
-          sleep 3
-          params = RedisInterface.new.get("dFile:processes:#{process_id}:params")
-        end
-        if params.nil?
-          Rails.logger.info "No parameters are set for #{process_id}, aborting!"
+      run = true
+      while run do
+        # Check if there are any running processes
+        running_keys = redis.keys("dFile:processes:*:state:running")
+        if running_keys && running_keys.count >= MAXIMUM_PROCESSES
+          Rails.logger.info "There are already processes running: #{running_keys.to_json} , aborting!"
           exit_fork
         end
 
-        process_object = {
-          id: process_id,
-          process: redis.get("dFile:processes:#{process_id}:process"),
-          params: JSON.parse(params),
-          priority: redis.get("dFile:processes:#{process_id}:priority")
-        }
-        queued_processes << process_object
+        # Check if there are any queued processes
+        queued_keys = redis.keys("dFile:processes:*:state:queued")
+        if queued_keys.empty?
+          Rails.logger.info "There are no queued processes, aborting!"
+          exit_fork
+        end
+
+        # Parse queued processes
+        queued_processes = []
+        queued_keys.each do |key|
+          process_id = redis.get(key)
+
+          params = redis.get("dFile:processes:#{process_id}:params")
+          if params.nil?
+            sleep 3
+            params = RedisInterface.new.get("dFile:processes:#{process_id}:params")
+          end
+          if params.nil?
+            Rails.logger.info "No parameters are set for #{process_id}, aborting!"
+            exit_fork
+          end
+
+          process_object = {
+            id: process_id,
+            process: redis.get("dFile:processes:#{process_id}:process"),
+            params: JSON.parse(params),
+              priority: redis.get("dFile:processes:#{process_id}:priority")
+          }
+          queued_processes << process_object
+        end
+
+        # Sort based on priority
+        queued_processes.sort! { |a,b| a[:priority] <=> b[:priority] }
+
+        # Choose the first process to run
+        @process = queued_processes.first
+
+        Rails.logger.info "Selected process for execution: #{@process.to_json}"
+
+        # Make sure process is still queued
+        if redis.get("dFile:processes:#{@process[:id]}:state:queued").nil?
+          Rails.logger.info "Process is no longer in queued state, aborting!"
+          exit_fork
+        end
+
+        redis.transaction do
+          # Remove queued key and create running key
+          redis.set("dFile:processes:#{@process[:id]}:state:running", @process[:id])
+          redis.del("dFile:processes:#{@process[:id]}:state:queued")
+
+        end #End redis transaction
+
+        Rails.logger.info "Staring process #{@process[:process]} for id: #{@process[:id]}"
+
+        # Run the process
+        case @process[:process]
+        when "CHECKSUM"
+          checksum(key_root: "dFile:processes:#{@process[:id]}:")
+        end
+
+        # Set state to done
+        redis.del("dFile:processes:#{@process[:id]}:state:running")
+        redis.set("dFile:processes:#{@process[:id]}:state:done", @process[:id])
+        redis.set("dFile:processes:#{@process[:id]}:state", "DONE")
+
+        Rails.logger.info "########### Process done! ############"
+
       end
-
-      # Sort based on priority
-      queued_processes.sort! { |a,b| a[:priority] <=> b[:priority] }
-
-      # Choose the first process to run
-      @process = queued_processes.first
-
-      Rails.logger.info "Selected process for execution: #{@process.to_json}"
-
-      # Make sure process is still queued
-      if redis.get("dFile:processes:#{@process[:id]}:state:queued").nil?
-        Rails.logger.info "Process is no longer in queued state, aborting!"
-        exit_fork
-      end
-
-      redis.transaction do
-        # Remove queued key and create running key
-        redis.set("dFile:processes:#{@process[:id]}:state:running", @process[:id])
-        redis.del("dFile:processes:#{@process[:id]}:state:queued")
-
-      end #End redis transaction
-
-      Rails.logger.info "Staring process #{@process[:process]} for id: #{@process[:id]}"
-
-      # Run the process
-      case @process[:process]
-      when "CHECKSUM"
-        checksum(key_root: "dFile:processes:#{@process[:id]}:")
-      end
-
-      # Set state to done
-      redis.del("dFile:processes:#{@process[:id]}:state:running")
-      redis.set("dFile:processes:#{@process[:id]}:state:done", @process[:id])
-      redis.set("dFile:processes:#{@process[:id]}:state", "DONE")
-
-      Rails.logger.info "########### Process done! ############"
-
-      # Run QueueManager again to continue picking processes from queue
-      QueueManager.new.run
-
-      exit_fork(:done)
     else
       # Parent process
       Process.detach(pid)
