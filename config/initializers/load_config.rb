cb47ea27d42a49ee23ad23871ecaba9516848155
Johan <johan.larsson@ub.gu.se>
2021-02-24 15:20:57 +0100

    moves paths to filevault.yml

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,18 +1,20 @@
 require 'yaml'
-
-# Load Redis config
 require "erb"
-require "yaml"
+
 redis_config_file =  'config/redis.yml'
-yaml = Pathname.new("#{Rails.root}/#{redis_config_file}")
-redis_config = YAML.load(ERB.new(yaml.read).result(binding))
-REDIS_CONFIG = redis_config.symbolize_keys #YAML.load(File.open( Rails.root.join("config/redis.yml")) ).symbolize_keys
-default_config = REDIS_CONFIG[:default].symbolize_keys
-config = default_config.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
-
+redis_yaml = Pathname.new("#{Rails.root}/#{redis_config_file}")
+redis_config = YAML.load(ERB.new(redis_yaml.read).result(binding))
+REDIS_CONFIG = redis_config.symbolize_keys
+reddis_default_config = REDIS_CONFIG[:default].symbolize_keys
+config = reddis_default_config.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
 $redis = Redis.new(config)
-
 # To clear out the db before each test
 $redis.flushdb if Rails.env == "test"
 
 
+filevault_config_file = 'config/filevault.yml'
+filevault_yaml = Pathname.new("#{Rails.root}/#{filevault_config_file}")
+filevault_config = YAML.load(ERB.new(filevault_yaml.read).result(binding))
+FILEVAULT_CONFIG = filevault_config.symbolize_keys
+filevault_default_config = FILEVAULT_CONFIG[:dfile_paths]
+Rails.configuration.dfile_paths = filevault_default_config
